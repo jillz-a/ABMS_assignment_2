265d3530cf938bae20a44552a2c4946b0a25a30b
LAAAAAARS <larsjensdijkstra@gmail.com>
2021-10-20 18:06:48 +0200

    Commit met fixed a/c spawn

--- cbs.py
+++ cbs.py
@@ -4,7 +4,7 @@ Implement CBS here
 
 import heapq
 import numpy as np
-import random
+import numpy.random as rnd
 from single_agent_planner import simple_single_agent_astar # , pop_node, push_node
 import pandas as pd
 import os
@@ -49,15 +49,11 @@ def detect_collisions(paths, id):
                     else:
                         # print('first collision = ', first_collision, type(first_collision[0][0]))
                         if type(first_collision[0]) == tuple:
-                            # print({'a1': agent0, 'a2': agent1, 'node': [first_collision[0][0], first_collision[0][1]],
-                            #      'timestep': first_collision[1]})
-                            # print(first_collision)
                             collision_list.append(
                                 {'a1': id[agent0], 'a2': id[agent1], 'node': [first_collision[0][0], first_collision[0][1]],
                                  'timestep': first_collision[1]})
                             return collision_list
                         else:
-                            # print({'a1': agent0, 'a2': agent1, 'node': [first_collision[0]], 'timestep': first_collision[1]})
                             collision_list.append(
                                 {'a1': id[agent0], 'a2': id[agent1], 'node': [first_collision[0]], 'timestep': first_collision[1]})
                             return collision_list
@@ -103,19 +99,40 @@ def pop_node(open_list):
 #Run Conflict Based Search program
 
 def run_CBS(aircraft_lst, nodes_dict, heuristics, t, constraints, dict_inverse_nodes):
-    # Importing the dictionary to go from (x,y) to the node number.
+    gate_nodes = [97, 34, 35, 36, 98]
+    gate_block_nodes = [97, 34, 35, 36, 98]
 
     numb_of_generated = 0
     open_list = []
-    # These four lines create a boolean for replanning. If no new aircraft enters the area then of course we do not need
-    # to replan.
+
+
+    # Sometimes an aircraft would spawn directly in front of another aircraft at a gate node. This results in the fact
+    # that two aicraft cannot plan their path, since aircraft are prohibited from moving backwards. The code loops
+    # through the active aircraft, and identifies their heading and current position. Aircraft that are currently at a
+    # node from which they cannot go backwards (node in gate_block_nodes) have right of way. In the next for loop,
+    # aircraft that want to spawn at the goal node of the other aircraft receive an update of their spawntime or spawn
+    # location.
+    blocked_list = []
+    for ac in aircraft_lst:
+        if ac.status == "taxiing":
+            dict = {"id": ac.id, "Heading": ac.heading, "Position": ac.path_to_goal[0][0], "Goal_node": ac.goal}
+            if dict["Heading"] == 270 and dict["Position"] in gate_block_nodes:
+                blocked_list.append(ac.goal)
+
     boolean = False
     for ac in aircraft_lst:
         if ac.spawntime == t:
+            if ac.start in blocked_list:
+                random_string = rnd.choice(["spawntime", "start_location"])
+                if random_string == "spawntime":
+                    ac.spawntime = ac.spawntime + 0.5
+                if random_string == "start_location":
+                    while ac.start in blocked_list:
+                        ac.start = rnd.choice(gate_nodes)
+                continue
             # The two lines below trigger the visualisation and correct position of the aircraft. The last line triggers
             # replanning.
             ac.status = "taxiing"
-            # print(ac.id, ac.goal)
             ac.position = nodes_dict[ac.start]["xy_pos"]
             boolean = True
 
