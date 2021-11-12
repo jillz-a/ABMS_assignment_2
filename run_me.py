0ecb232f8601fbffb6286e56838eaf980899099f
jilles <jilles@ziggo.nl>
2021-11-12 14:26:28 +0100

    Gate blokking implemented, (mostly) works

--- run_me.py
+++ run_me.py
@@ -203,7 +203,7 @@ escape_pressed = False
 time_end = simulation_time + 5
 dt = 0.1 #should be factor of 0.5 (0.5/dt should be integer)
 t= 0
-random = True #True uses randomly generated aircraft, False generates 2 aircraft which collide at t = 5.0
+random = False #True uses randomly generated aircraft, False generates 2 aircraft which collide at t = 5.0
 print("Simulation Started")
 while running:
     t= round(t,2)    
@@ -288,8 +288,8 @@ while running:
     # Spawn aircraft for this timestep (used for verification purposes).
     #These aircraft are set to collide at node 14 at t = 5.0
     if t == 1 and random == False:
-        ac = Aircraft(0, 'A', 37,34, 1.5, nodes_dict) #As an example we will create one aicraft arriving at node 37 with the goal of reaching node 36
-        ac1 = Aircraft(1, 'A', 38,97, 1.0, nodes_dict)#As an example we will create one aicraft arriving at node 36 with the goal of reaching node 37
+        ac = Aircraft(0, 'A', 37,97, 1.0, nodes_dict) #As an example we will create one aicraft arriving at node 37 with the goal of reaching node 36
+        ac1 = Aircraft(1, 'D', 97,37, 4.0, nodes_dict)#As an example we will create one aicraft arriving at node 36 with the goal of reaching node 37
         aircraft_lst.append(ac)
         aircraft_lst.append(ac1)
 
